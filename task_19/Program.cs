2befac8301a884736a46c111771641b7127105e3
CJINBNH <alex_egorov_89@mail.ru>
2022-08-17 21:12:07 +0400

    Completed task_19

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом (12821 -> да)
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+string numberToString = number.ToString();
+int stringLength = numberToString.Length;
+
+if (stringLength == 5)
+{
+    if (numberToString[0] == numberToString[4] && numberToString[1] == numberToString[3])
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Число должно быть пятизначным");
+}
+
+// while (stringLength == 5)
+// {
+// for (int i = 0; i < 2; i++)
+// {
+//     while (numberToString[i] == numberToString[(stringLength-1)-i])
+//     {
+//         Console.WriteLine($"Число {number} является палиндромом");
+//     }
+// }
+// } 
+// if (stringLength != 5)
+// {
+//     Console.WriteLine("Число должно быть пятизначным");
+// }
