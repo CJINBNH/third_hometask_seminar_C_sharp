79f58b73f35bcca88747a2a17dadaee86443c030
CJINBNH <alex_egorov_89@mail.ru>
2022-08-18 20:49:55 +0400

    Added the solution of task021 using cycle

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -4,6 +4,7 @@
 
 // Формула вычисления расстояния между двумя точками в пространстве
 // AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+Console.Clear();
 Console.WriteLine("Введите координату x1 первой точки: ");
 double x1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату y1 первой точки: ");
@@ -20,5 +21,18 @@ double z2 = Convert.ToInt32(Console.ReadLine());
 
 // double length = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1) + (z2-z1)*(z2-z1));
 
-double length = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow( y2 - y1, 2) + Math.Pow( z2 - z1, 2));
-Console.WriteLine(length);
+// double length = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow( y2 - y1, 2) + Math.Pow( z2 - z1, 2));
+// Console.WriteLine(length);
+
+// добавить в решение функцию
+
+double distance = 0;
+double Length(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow( y2 - y1, 2) + Math.Pow( z2 - z1, 2));
+    return distance;
+}
+
+
+distance = Length(x1, y1, z1, x2, y2, z2);
+Console.WriteLine(distance);
